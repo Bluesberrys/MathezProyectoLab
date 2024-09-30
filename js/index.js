f0ebe55153b31312ea22edea59b5eac1cb11254b
Bluesberrys <bluesberrysden@gmail.com>
2024-09-30 09:18:37 -0600

    add responsive

--- js/index.js
+++ js/index.js
@@ -136,16 +136,16 @@ document.addEventListener("DOMContentLoaded", function () {
   // Check for saved dark mode preference
   if (localStorage.getItem("darkMode") === "enabled") {
     body.classList.add("dark-mode");
+    darkModeToggle.checked = true;
   }
 
   // Toggle dark mode
-  darkModeToggle.addEventListener("click", function () {
-    body.classList.toggle("dark-mode");
-
-    // Save preference to localStorage
-    if (body.classList.contains("dark-mode")) {
+  darkModeToggle.addEventListener("change", function () {
+    if (this.checked) {
+      body.classList.add("dark-mode");
       localStorage.setItem("darkMode", "enabled");
     } else {
+      body.classList.remove("dark-mode");
       localStorage.setItem("darkMode", null);
     }
   });
